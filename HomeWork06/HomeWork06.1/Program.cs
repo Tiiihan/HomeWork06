3904377fc9ef7e085f4fe9fd6a4a9c03c38d706c
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-19 17:34:14 +0300

    Resolved task 1

--- HomeWork06/HomeWork06.1/Program.cs
+++ HomeWork06/HomeWork06.1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isEqual = false;
+int counter = 0;
+double averageSalary = 0f;
+
+Console.Write("Enter quantity of employers");
+int employers = int.Parse(Console.ReadLine());
+
+
+
+for (int counter2 = 0; counter2 < employers; counter2++)
+{
+	Console.Write($"Enter salary of {counter2 + 1}th employee ");
+	double salaryPerMonth = float.Parse(Console.ReadLine());
+
+	averageSalary += salaryPerMonth;
+}
+
+Console.WriteLine($"Average salary per month is {averageSalary/employers}");
+
+
+//bool isEqual = false;
+//while (!isEqual)
+//{
+//    Console.Write($"Enter salary of {counter + 1}th employee ");
+//	double salaryPerMonth = float.Parse(Console.ReadLine());
+
+//    counter++;
+//    averageSalary += salaryPerMonth;
+
+//    if (counter == employers)
+//        isEqual = true;
+//}
