18782809a7965ef786e6e5715bb63f1a6ab1bebb
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-19 17:41:30 +0300

    Resolved task 2

---
+++ HomeWork06/HomeWork06.2/Program.cs
@@ -0,0 +1,10 @@
+﻿string stars = "*";
+
+Console.Write("Enter the number of rows of graphics");
+int numberOfRows = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfRows; i++)
+{
+    Console.WriteLine(stars);
+    stars += "*";
+}
