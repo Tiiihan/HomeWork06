23204095f5cea81bd104e74c4f55f9e650d1450f
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-21 18:16:00 +0300

    Resolved task 4

---
+++ HomeWork06/HomeWork06.4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Перевірка паролю: Напишіть програму, яка перевіряє пароль користувача.
+//Пароль вважається прийнятним, якщо він містить принаймні 8 символів, з яких принаймні один символ - це цифра,
+//а також принаймні один символ - це спеціальний символ (наприклад, !, @, # тощо).
+
+string userPassword = "";
+bool isHasSpecailChars = false;
+
+while (userPassword.Length < 8 || !isHasSpecailChars)
+{
+	Console.Write("Enter password: ");
+	userPassword = Console.ReadLine();
+
+	isHasSpecailChars = false;
+
+	for (int i = 0; i < userPassword.Length; i++)
+	{
+		if (userPassword[i] == '!' || userPassword[i] == '@')
+			isHasSpecailChars = true;
+	}
+}
