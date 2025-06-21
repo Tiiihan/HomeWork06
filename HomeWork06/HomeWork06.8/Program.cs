4491f9b90933d8d826c8992c8c37c395a5647c01
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-21 18:19:20 +0300

    Resolved task 8

---
+++ HomeWork06/HomeWork06.8/Program.cs
@@ -0,0 +1,23 @@
+﻿//Перевірка на простоту: Напишіть програму, яка перевіряє, чи є задане користувачем число простим. 
+//Число вважається простим, якщо воно ділиться лише на 1 і на себе.
+
+bool isInt = false;
+int userInput = 0;
+int counter = 0;
+
+while (!isInt)
+{
+	Console.Write("Enter number: ");
+	isInt = int.TryParse(Console.ReadLine(), out userInput);
+}
+
+for (int i = 2; i <= userInput; i++)
+{
+	if (userInput % i == 0 && i != userInput)
+		counter++;
+}
+
+if (counter == 0)
+	Console.WriteLine($"It`s prime {userInput}");
+else
+	Console.WriteLine("It`s not prime");
