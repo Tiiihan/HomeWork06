2bf0a507e657f78cce02791b6813333d98ca4c9a
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-21 18:18:30 +0300

    Resolved task 7

---
+++ HomeWork06/HomeWork06.7/Program.cs
@@ -0,0 +1,16 @@
+﻿//Генерація таблиці множення для конкретного числа: 
+//Напишіть програму, яка запитує в користувача число, а потім генерує таблицю множення для цього числа від 1 до 10.
+
+const int lowLIMIT = 1;
+const int upperLIMIT = 10;
+bool isInt = false;
+int userInput = 0;
+
+while (!isInt)
+{
+	Console.Write("Enter number: ");
+	isInt = int.TryParse(Console.ReadLine(), out userInput);
+}
+
+for(int i = lowLIMIT; i <= upperLIMIT; i++)
+    Console.WriteLine(i * userInput);
