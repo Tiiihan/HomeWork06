4e004a7fe2d5941d70346bca768427ca345eda2d
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-21 18:12:33 +0300

    Resolved task 2

--- HomeWork06/HomeWork06.2/Program.cs
+++ HomeWork06/HomeWork06.2/Program.cs
@@ -1,10 +1,18 @@
-﻿string stars = "*";
+﻿//Побудова графіку зірочками: Напишіть програму, яка будує графік за допомогою зірочок.
+//Користувач повинен ввести кількість рядків графіка. Кожен рядок графіка має містити відповідну кількість зірочок.
 
-Console.Write("Enter the number of rows of graphics");
-int numberOfRows = int.Parse(Console.ReadLine());
+bool isInt = false;
+int numberOfRows = 0;
+string stars = "*";
+
+while (!isInt)
+{
+	Console.Write("Enter the number of rows of graphics: ");
+	isInt = int.TryParse(Console.ReadLine(), out numberOfRows);
+}
 
 for (int i = 0; i < numberOfRows; i++)
 {
-    Console.WriteLine(stars);
-    stars += "*";
+	Console.WriteLine(stars);
+	stars += "*";
 }
